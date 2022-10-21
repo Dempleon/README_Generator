75f2a0f7d3a57a9b4782a9b54a061f520cc106f0
Dempoleon <dempoleon@gmail.com>
2022-10-20 22:14:17 -0700

    wrote the question objects to be used as prompts

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of the application?',
+        name: 'projectName'
+    },
+    {
+        type: 'input',
+        message: 'What is the application description?',
+        name: 'desccription'
+    }, 
+    {
+        type: 'input',
+        message: 'How do users install the application?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'How do users use the application',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'How do other developers contribute to the application?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'How will the application be tested?',
+        name: 'testing'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
